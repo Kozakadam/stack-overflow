4ddf30dac17c2c5b51ce39e27511b8a5ef66df72
Kozakadam <kar.adam.robert@gmail.com>
2024-01-08 10:53:18 +0100

    create base structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import "./App.css";
+import SearchPage from "./pages/SearchPage";
+import ProfilePage from "./pages/ProfilePage";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([{
+  path: "/",
+  children: [
+    {
+      path: "",
+      element: (
+        <HomePage/>
+      )
+    },
+    {
+      path: "search",
+      element: (
+        <SearchPage/>
+      )
+    },
+    {
+      path: "profile",
+      element: (
+        <ProfilePage/>
+      )
+    }
+  ]
+}]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
